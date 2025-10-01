5cc18fe43e979471b0c61f65aab25e2e917bd281
KrissAsterisk <crisrogamer@gmail.com>
2025-10-01 22:39:31 +0300

    File Manipulation
    almost done working with files;
    main focus was to learn syntax and get used to py and its "indentation"

--- PythonStartup.py
+++ PythonStartup.py
@@ -1,31 +1,31 @@
-
-
 import re
 import os
-
-
-boolean = True     
+     
 class file_manipulation:
 
     def create_file(file_path): #file_path will be the name of the file
         try:
-            with open(file_path, "x") as file:
-                file.close()
+            open(file_path, "x")
+                
         except Exception as e:
             print(f"Error: {e}")
     
     def delete_file(file_path): #file_path = name
         try:
-            with open(file_path, "r"):
                 os.remove(file_path)
+                print("Deletion complete.")
         except Exception as e:
             print(f"Error: {e}")
-
+        
     def read_file(file_path):
         try:
             with open(file_path, "r") as file:
                 content = file.read()
-            return content
+                if(content == ""):
+                    return("This file is empty")
+                else: 
+                    return content    
+
         except Exception as e:
             print(f"Error: {e}")
 
@@ -48,7 +48,7 @@ class file_manipulation:
                     else:
                         word_dictionary[word] = 1
                 for word, occurence in sorted(word_dictionary.items(), key=lambda x: (-x[1], x[0])): #.items - (key,value) tuples | sorted key - use value from tuple for sorting | lambda = anonymous func - x[0] is the key x[1] is the value | negative x[1] - sort by descending value first | non-negative x[0] - sort keys alphabetically second
-                    print(f"{word}: {occurence}")
+                    print(f"{word}: {occurence}") #TODO: Implement a sorting algorithm for maximum style points
             
         except Exception as e:
             print(f"Error: {e}")
@@ -72,14 +72,7 @@ class main:
     while(True):
         os.system("CLS")
         dir.read_dir()
-        file_choice = input("Input a File name or Directory:\n\n>>> ").strip()
-        
-        #Asks what to do & shows current dir
-        #User inputs something irrlevat - nothing happens
-        #User inputs file or directory - it checks whether its a directory
-        #if yes: it goes to that directory
-        #if no: it stores the path of the file
-        #with the path of the file, the user can now read/write/etc.
+        file_choice = input("Input a File name / Directory:\n\n>>> ").strip() # revise this
 
         match file_choice[:4]: 
 
@@ -87,13 +80,13 @@ class main:
                 exit()
 
             case "crea": #create files
-                while True:
-                    dir.read_dir()
-                    file_name = input("Enter the name of the file (exit to quit): ")
-                    if(file_name == 'exit'):
-                        break
-                    file_manipulation.create_file(file_name)
+                file_name = input("Enter the name of the file to be created: ")
+                file_manipulation.create_file(file_name)
+                if(file_name.endswith(".txt")):
+                    print("You've created a text file!")
 
+            case "make": # TBD: make dirs
+                pass
 
             case _: # manipulate files or enter dir
                 if(os.path.isfile(file_choice)):
@@ -108,9 +101,9 @@ class main:
                                 file_manipulation.overwrite_file(file_choice, input("Input: "))  
                             case "anal": # analyze file
                                 file_manipulation.analyze_file(file_choice)
-                            case "dele": # delete file (doesn't work because youre accesing the file....)
+                            case "dele": # delete file 
                                 file_manipulation.delete_file(file_choice)
-
+                                break
                             case _:
                                 print("Invalid Option.")
 
